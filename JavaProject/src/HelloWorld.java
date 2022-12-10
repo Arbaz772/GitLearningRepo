edf8828d246b60844556ba0cd723bbf6ab5c3fbf
Arbaz <syedarbaz772@gmail.com>
2022-12-10 20:01:16 +0530

    Check reset

--- JavaProject/src/HelloWorld.java
+++ JavaProject/src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World Program");
         HelloGit.printHelloGit();
+        System.out.println("Checking git reset");
     }
 }
