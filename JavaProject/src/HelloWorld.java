d176b05a90c8430b0cabb575d044a3815ab9ef51
Arbaz <syedarbaz772@gmail.com>
2022-12-10 19:58:00 +0530

    Revert "sout of Git Stash"
    This reverts commit 1a804463a27a72fb78af930390bb366a4f93f559.

--- JavaProject/src/HelloWorld.java
+++ JavaProject/src/HelloWorld.java
@@ -1,9 +1,6 @@
-import java.sql.SQLOutput;
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello World Program");
         HelloGit.printHelloGit();
-        System.out.println("Git Stash Check");
     }
 }
